796c3a44fc9d827e9124af2658367f3e2e576971
Bar Leshem <barlesh8@gmail.com>
2022-07-13 18:06:40 +0300

    comments

--- client/recaptcha-v2-login-page/src/App.js
+++ client/recaptcha-v2-login-page/src/App.js
@@ -3,34 +3,10 @@ import './App.css';
 import Login from './pages/Login';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit this <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
   return (
     <div className="App">
-    {/* <Routes>
-      <Route path="/" element={<Login />} />
-    </Routes>    */}
       <div>
-        {/* <h1>Login</h1> */}
-        {/* <Route path="/login"> */}
           <Login />
-        {/* </Route> */}
       </div>
   </div>
   );
